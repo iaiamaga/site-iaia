e0fd05c6ac0bf0df5ddd1d919fd864e46265b6f8
Magaiaia <magaiaiabubar@gmail.com>
2026-02-10 01:20:35 -0300

    add Designs na seção design.

--- src/components/molecules/DesignGallery.tsx
+++ src/components/molecules/DesignGallery.tsx
@@ -2,24 +2,31 @@ import { type GalleryImage } from "../atoms/ImageGallery"
 
 export const DESIGN_IMAGES: GalleryImage[] = [
     {
-        src: 'https://images.unsplash.com/photo-1534353436294-0dbd4bdac845?q=80&w=800&auto=format&fit=crop',
-        href: 'https://images.unsplash.com/photo-1534353436294-0dbd4bdac845',
-        alt: 'A clean and minimalist workspace with a laptop and plants',
-        title: 'Clean Workspace',
-        description: 'A modern and minimalist desk setup for focus and creativity.'
+        src: './src/assets/designs/wireframes/wireframe-apphealth.png',
+        href: '#',
+        alt: 'Um wireframe de um aplicativo de atividade fisica',
+        title: 'Wireframe alta fidelidade - HealthApp',
+        description: 'Um wireframe de alta fidelidade que fiz nop processo de um curso de UI e UX design. Esse design foi particularmente desafiador porque eu ainda não sabia nada de figma e na hora de fazer o protótipo de fato deu tudo errado!! kkkkkk mas valeu muito porque hoje em dia eu já fico menos perdida para fazer protótipos.'
     },
     {
-        src: 'https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?q=80&w=800&auto=format&fit=crop',
-        href: 'https://images.unsplash.com/photo-1496171367470-9ed9a91ea931',
-        alt: 'A responsive website design shown on a laptop, tablet, and phone',
-        title: 'Digital Presence',
-        description: 'Showcasing a responsive website design across multiple devices.'
+        src: './src/assets/designs/wireframes/domingueira-tribute-bobmarley-im2b(v3).png',
+        href: '#',
+        alt: 'Flyer para show do tributo a Bob Marley',
+        title: 'Flyer evento: Tributo a Bob Marley',
+        description: 'Esse é o design mais recente que eu fiz. foi muito bom para refinar minhas habilidades de espaçamento e encaixar as informações e a imagem de forma harmonica. TAMBÉM foi muito legal descobrir como deixar a imagem com esse efeito colorido que aprendi a fazer no gimp, isso me deixou super feliz. amo amo amo gimp e softwares open-source que salvam a vida dos artistas digitais! (Também amo as musicas do Bob Marley, aliás enquanto fazia o design aproveitei para ver um pouco da história do visual do reggae e do reggae em si, interessante demais, qualquer dia escrevo mais sobre)'
     },
     {
-        src: 'https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=800&auto=format&fit=crop',
-        href: 'https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e',
-        alt: 'Branding materials including a notebook, pen, and coffee cup with a logo',
-        title: 'Brand Identity',
-        description: 'A cohesive collection of branding materials, from notebook to coffee cup.'
+        src: './src/assets/designs/wireframes/wireframe-lojavirtual.png',
+        href: '#',
+        alt: 'Um wireframe de baixa fidelidade para uma loja virtual',
+        title: 'Wireframe de baixa fidelidade - Kamurima Loja Virtual',
+        description: 'Um wireframe de baixa fidelidade que fiz no processo do mesmo curso de UI e UX design. Esse foi literalmente o primeiro que fiz na vida, e até hoje eu AMO... ta tem algumas coisinhas para concertar mas po ficou mo estiloso, eu super compraria dessa loja.'
+    },
+    {
+        src: './src/assets/designs/wireframes/design-desconferencia404-coodinations.jpg',
+        href: '#',
+        alt: 'Post de conexão e coordenação global com estetica cybernetica',
+        title: 'Post Desconferência 404: Coordinations',
+        description: 'Esse foi literalmente uma experiência fazer. Eu estava no meu primeiro hackathon no evento EthLatam em São Paulo fazendo esse (e outros) designs para a Desconferência404 (muito interessante aliás) que ocorreu dentro do mesmo evento. Achei interessante pois lidei com uma certa pressão e insegurança se eu ia fazer algo decente. Mas eu achoque foi missão super cumprida!'
     },
 ];
